bca799d19aed8229d7895ef42885a484940e67e9
Azizbe14k <147371225+Azizbe14k@users.noreply.github.com>
2023-11-14 19:55:27 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,15 +1,14 @@
 float problemSolution1(float consumed_water) {
     float cost;
     // write your code here
-    std::cin >> consumed_water;
     if (consumed_water <= 30){
-        std::cout << cost = 13 + consumed_water * 0.4;
+        cost = 13 + consumed_water * 0.4;
     }else if (consumed_water > 30 and consumed_water <= 50){
-        std::cout << cost = 13 + 30 * 0.4 + (consumed_water - 30) * 0.12;
+        cost = 13 + 30 * 0.4 + (consumed_water - 30) * 0.12;
     }else if (consumed_water > 50 and consumed_water <= 55){
-        std::cout << cost = 13 + 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
+        cost = 13 + 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
     }else if (consumed_water > 55){
-        std::cout << cost = 13 + 30 * 0.4 + 20 * 0.12 + 5 * 1.4 + (consumed_water - 55) * 1.5;
+        cost = 13 + 30 * 0.4 + 20 * 0.12 + 5 * 1.4 + (consumed_water - 55) * 1.5;
     }
     return cost;
 }
