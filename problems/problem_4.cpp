2f1d319950c1ac3c3cc9f8ff4bb234e8e187a977
Azizbe14k <147371225+Azizbe14k@users.noreply.github.com>
2023-11-14 21:22:15 +0500

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,7 +3,25 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
+    std::stringstream ss(macAddress);
+    std::string octet;
+    std::getline(ss, octet, ':');
+    std::string result;
+
+    int firstOctet;
+    ss.clear();
+    ss.str(octet);
+    ss >> std::hex >> firstOctet;
+
+    if (firstOctet % 2 == 0) {
+        result = "Unicast";
+    }else if (firstOctet % 2 != 0) {
+        result = "Multicast";
+    }else if (macAddress == "FF:FF:FF:FF:FF:FF") {
+        result = "Broadcast";
+    }
 
     // make use of control flow statements
     // return result;
+return result;
 }
