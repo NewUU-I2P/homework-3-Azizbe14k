5bf3f04dd246bd71d7b432ba50f739146d2f9621
Azizbe14k <147371225+Azizbe14k@users.noreply.github.com>
2023-11-14 19:53:35 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,7 +1,6 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here;
-    std::cin >> i >> j >> k;
     result=(i > j and j > k) ? i: (j > i and j > k) ? j: k;
 
     return result;
