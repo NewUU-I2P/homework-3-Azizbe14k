c836bc3db3e14b40b66085686d49e131c0fc0f43
Azizbe14k <147371225+Azizbe14k@users.noreply.github.com>
2023-11-14 18:38:56 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -7,9 +7,8 @@ float problemSolution1(float consumed_water) {
         cout<<cost=13+30*0.4+(consumed_water-30)*0.12;
     }else if(consumed_water>50 and consumed_water<=55){
         cout<<cost=13+30*0.4+20*0.12+(consumed_water-50)*1.4;
-    }else if(consumed_water>55){
+    }else
         cout<<cost=13+30*0.4+20*0.12+5*1.4+(consumed_water-55)*1.5;
-    }
 
     return cost;
 }
