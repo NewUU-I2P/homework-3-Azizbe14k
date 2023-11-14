8c33062e5a515a94dd8c605935c115624f841282
Azizbe14k <147371225+Azizbe14k@users.noreply.github.com>
2023-11-14 19:01:16 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,6 +1,7 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here;
+    result=(i > j and j > k) ? i: (j > i and j > k) ? j: k;
 
     return result;
 }
